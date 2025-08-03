de0e1d72b90191bfb179f2003f963e024accb16c
猫派派 <zhouqujiang@gmail.com>
2025-08-03 13:40:51 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <cstdio>
 
 #include "PointIsInCircle.h" // PointIsInCircle()
-#include "train_fanqi.h" // FanqiTrain()
-#include "train_tic_tac_toe.h" // TrainTicTacToe()
+//#include "train_fanqi.h" // FanqiTrain()
+//#include "train_tic_tac_toe.h" // TrainTicTacToe()
 #include "train_pic_classification.h" // TrainPic()
 #include "nn_function.h"
-#include "play_tic_tac_toe.h" // Play()1 ÎªµçÄÔÏÈÊÖ£¬2 ÎªµçÄÔºóÊÖ
+//#include "play_tic_tac_toe.h" // Play()1 ä¸ºç”µè„‘å…ˆæ‰‹ï¼Œ2 ä¸ºç”µè„‘åæ‰‹
 
 
 int main() {
@@ -13,4 +13,5 @@ int main() {
 	FanqiTrain();
 
 	return 0;
+
 }
