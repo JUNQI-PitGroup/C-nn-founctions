09343e85af6a5d8412d79f041ceddf41f89239b5
猫派派 <zhouqujiang@gmail.com>
2025-08-03 13:52:23 +0800

    Update and rename PointIsInCircle.cpp to PointIsInCircle.c

--- PointIsInCircle.c
+++ PointIsInCircle.c
@@ -25,12 +25,12 @@ void PointIsInCircle() {
             labelTensor[i][0] = 0; // Outside the circle
 		}
 	}
-    // ¿É×Ô¶¨Òå²ÎÊı
+    // å¯è‡ªå®šä¹‰å‚æ•°
     const int batchSize = batchNum;
     const float initialLearningRate = 0.01;
     const int initialEpoch = 200;
     const int patience = 200;
-    const float learningRateDecayTo = 0.5; // Ñ§Ï°ÂÊË¥¼õµ½Ô­À´µÄ learningRateDecayTo ±¶
+    const float learningRateDecayTo = 0.5; // å­¦ä¹ ç‡è¡°å‡åˆ°åŸæ¥çš„ learningRateDecayTo å€
 
     for (int i = 0; i < batchSize; i++) {
         float floatInputTensor[inputLength] = { 0 };
